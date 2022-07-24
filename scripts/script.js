24576b4481395a8d31e4fd6bedfb5bb9e384c18f
George <george.lmfao@gmail.com>
2022-07-24 13:24:01 -0700

    Installed JSDoc, generated a folder and html file

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+/*
+  @description
+  title: A Title 
+  file: script.js
+  This is the description of the script.js file, which is the main 
+  code file that calls other *.js files. 
+*/
+
+/*
+  @todo Finish the HTML document
+*/
+
+// @todo fill the Elements object
+const Elements = {};
+
+// @todo fill the Listeners object
+const Listeners = {};
+
+// @todo fill the Functions object
+const Functions = {};
